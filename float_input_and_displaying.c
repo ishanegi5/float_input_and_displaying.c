a7e342c06969ab7ac0cb8def5c463cc9b14413c6
Isha Negi <itsishanegi@gmail.com>
2024-11-24 13:56:46 +0530

    Create float_input_and_displaying.c

---
+++ float_input_and_displaying.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    float a,b,c;
+    printf("Enter a= ");
+    scanf("%f",&a);
+    printf("Enter b= ");
+    scanf("%f",&b);
+    c=a*b;
+    printf("The product of a & b is: %f",c);
+    printf("\nName: Isha Negi \nRoll number: 24 CE 47\n");
+}
